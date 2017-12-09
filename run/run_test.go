422f91a8cc7d4ea90cc1c09499d7e541c67a8a8a
Guohao Dou <gdou2@illinois.edu>
2017-12-08 23:59:33 -0600

    testing

--- run/run_test.go
+++ run/run_test.go
@@ -2,18 +2,45 @@ package run_test
 
 import (
 	"os"
+	"fmt"
+	"gopkg.in/mgo.v2"
+	// "os"
 	"testing"
-
+	"log"
+	"gopkg.in/ory-am/dockertest.v3"
 	"github.com/user/numb/run"
 )
 
-func TestRun(t *testing.T) {
-	run.Train("mkdir temp", nil)
-	if _, err := os.Stat("temp"); os.IsNotExist(err) {
-		t.Error("dir not really created!")
+var db *mgo.Session
+
+func TestMain(m *testing.M) {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalln("Can't connect to docker")
 	}
-	run.Test("rmdir temp", map[string]interface{}{"silent": true})
-	if _, err := os.Stat("temp"); os.IsExist(err) {
-		t.Error("dir not really removed!")
+	resource, err := pool.Run("sameersbn/mongodb", "latest", []string{})
+	if err != nil {
+		log.Fatalln("Can't get resource")
 	}
+	if err := pool.Retry(func () error {
+		var err error
+		db, err = mgo.Dial(fmt.Sprintf("localhost:%s", resource.GetPort("27017/tcp")))
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+	code := m.Run() // actually run testcases
+	err = pool.Purge(resource)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	os.Exit(code)
+}
+
+func TestTrain(t *testing.T) {
+	os.Chdir("testcases")
+	run.Train("python train.py")
 }
